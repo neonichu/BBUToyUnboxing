a711c0d5f103754e3780c724699ad8c618759cd6
Boris Bügling <boris@icculus.org>
2014-07-03 11:55:56 +0200

    Added a quick installation script.

---
+++ Scripts/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+FRAMEWORK_URI=https://github.com/neonichu/ContentfulDeliveryAPIFramework/releases/download/1.2.0/ContentfulDeliveryAPI.tar.gz
+PLAYGROUND_DIR="${HOME}/Library/Developer/Playground Frameworks"
+#PLAYGROUND_URI=
+PLUGIN_URI=https://github.com/neonichu/BBUToyUnboxing/releases/download/0.5/BBUToyUnboxing.xcplugin.tar.gz
+PLUGINS_DIR="${HOME}/Library/Application Support/Developer/Shared/Xcode/Plug-ins"
+
+echo "Installing Xcode plug-in..."
+mkdir -p "${PLUGINS_DIR}" &&
+curl -L $PLUGIN_URI | tar xvz -C "${PLUGINS_DIR}"
+
+echo "Downloading ContentfulDeliveryAPI.framework..."
+mkdir -p "${PLAYGROUND_DIR}" &&
+curl -L $FRAMEWORK_URI | tar xvz -C "${PLAYGROUND_DIR}"
+
+#echo "Downloading Contentful Documentation Playground..."
+#curl -L $PLAYGROUND_URI | tar xvfz -C "${HOME}/Desktop"
+
+echo -e "\nInstallation successful.\nPlease restart your Xcode and open the Playground after ~5 seconds."
